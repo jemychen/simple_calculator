1a40b44af714df135a397da8aa1f8e7cee0ce84b
Jemy Chen <jemychen@live.com>
2025-07-21 21:28:47 -0400

    Added window event listener

--- index.js
+++ index.js
@@ -87,3 +87,20 @@ clearLastEl.addEventListener('click', () => {
     dis2Num = ''
 })
 
+window.addEventListener('keydown', (e) => {
+    if (
+        e.key === '0' ||
+        e.key === '1' ||
+        e.key === '2' ||
+        e.key === '3' ||
+        e.key === '4' ||
+        e.key === '5' ||
+        e.key === '6' ||
+        e.key === '7' ||
+        e.key === '8' ||
+        e.key === '9' ||
+        e.key === '.'
+    ) {
+        clickButton(e.key)
+    }
+})
